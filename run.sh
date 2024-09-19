de17b2d8e99af1d72f95b565614d949d0a0397f9
Jonathan Lewis <47508347+JMANY42@users.noreply.github.com>
2024-09-19 18:17:28 -0500

    Add files via upload

--- run.sh
+++ run.sh
@@ -1,2 +1,39 @@
-docker run   -e "DISPLAY=$DISPLAY"   -v "$HOME/.Xauthority:/root/.Xauthority:ro"   --network host -it *whatever the container name is*
+#!/usr/bin/bash
 
+CONTAINER_NAME="RoboBoat"
+IMAGE_NAME="roboboat"
+IMAGE_TAG="latest"
+
+WORK_DIR="$HOME/code/python/opencv-playground/src"
+
+CONTAINER_ID=`docker ps -aqf "name=^/${CONTAINER_NAME}$"`
+
+if [ -z "${CONTAINER_ID}" ]; then
+
+    docker run \
+        --tty \
+        --detach \
+        --name ${CONTAINER_NAME} \
+        --device "/dev/dri:/dev/dri" \
+        --privileged \
+        --gpus all \
+        --shm-size 16G \
+        --network host \
+        --env "DISPLAY=$DISPLAY" \
+        --volume "/tmp/.X11-unix:/tmp/.X11-unix" \
+        --volume "${WORK_DIR}:/root/code/src/" \
+       ${IMAGE_NAME}:${IMAGE_TAG} 
+
+else
+    xhost +local:`docker inspect --format='{{ .Config.Hostname }}' ${CONTAINER_ID}`
+
+    if [ -z `docker ps -qf "name=^/${CONTAINER_NAME}$"` ]; then
+        echo "${CONTAINER_NAME} container not running. Starting container..."
+        docker start ${CONTAINER_ID}
+    else
+        echo "Attaching to running ${CONTAINER_NAME} container..."
+    fi
+    docker exec -it ${CONTAINER_ID} bash
+
+    xhost -local:`docker inspect --format='{{ .Config.Hostname }}' ${CONTAINER_ID}`
+fi
